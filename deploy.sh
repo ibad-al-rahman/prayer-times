f5700c1928612a0cc49683ccaa8e16dd71b7c6e7
Ghamza-Jd <hamzah.jadeed@gmail.com>
2024-12-01 00:14:34 +0200

    feat: added deployment script

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Setup
+git switch -c deployment
+years=(2024)
+echo "Years: ${years[@]}"
+
+# Build step
+mkdir -p temp
+shopt -s dotglob
+for item in *; do
+    if [ "$item" != "temp" ] && [ "$item" != ".git" ]
+    then
+        mv "$item" temp/
+    fi
+done
+shopt -u dotglob
+
+for y in "${years[@]}"
+do
+    echo "Deploying year: $y"
+    ptig v1 --year $y --input "temp/$y" --output ./ --format csv
+done
+
+# Deployment step
+rm -rf temp
+ls -lart
+
+git config --global user.name "Ghamza-Jd"
+git config --global user.email "hamzah.jadeed@gmail.com"
+git add .
+git commit -m "Deploying years: ${years[@]}"
+git push origin deployment --force
